0c66aa4c777c873a1db6b4b7d59f68989ca90584
Piinn <lin_loveorm@hotmail.com>
2018-07-10 19:03:47 +0700

    no message

--- src/components/MenuIcons.js
+++ src/components/MenuIcons.js
@@ -1,23 +1,37 @@
 import React from 'react';
 import { Grid, Image } from 'semantic-ui-react';
 import _ from 'lodash';
-import exIcon from '../assets/pics/images.png';
+import register from '../assets/pics/register.png';
+import fyi from '../assets/pics/fyi.png'
+import develop from '../assets/pics/develop.png'
+import evaluation from '../assets/pics/evaluation.png'
+import welfare from '../assets/pics/welfare.png'
+import growth from '../assets/pics/growth.png'
 
 const menuIcons = (props) => {
-    const numOfIcons = 5;
+    const numOfIcons = 6;
     const width = Math.floor(16 / numOfIcons)
-    const menuColumns = _.times(numOfIcons, (idx) => {
-        return (
-            <Grid.Column key={ idx } width={ width }>
-                <Image src={ exIcon } />
-            </Grid.Column>
-        )
-    });
-
-    return (
+      return (
         <Grid container stackable verticalAlign='middle' >
             <Grid.Row>
-                { menuColumns }
+                <Grid.Column width={ width }>
+                    <Image src={ register } />
+                </Grid.Column>
+                <Grid.Column width={ width }>
+                    <Image src={ fyi } />
+                </Grid.Column>
+                <Grid.Column width={ width }>
+                    <Image src={ develop } />
+                </Grid.Column>
+                <Grid.Column width={ width }>
+                    <Image src={ evaluation } />
+                </Grid.Column>
+                <Grid.Column width={ width }>
+                    <Image src={ welfare } />
+                </Grid.Column>
+                <Grid.Column width={ width }>
+                    <Image src={ growth } />
+                </Grid.Column>
             </Grid.Row>
         </Grid>
     );
