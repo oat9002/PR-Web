7ce330c578805020cbd761c76de9926182578ef4
Sarun Rakwijitsil <Sarun.Rakwijitsil@agoda.com>
2018-07-11 22:36:43 +0700

    Added carousel

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import {
     Button,
     Container,
@@ -16,28 +16,40 @@ import {
     GridRow,
     Transition
   } from 'semantic-ui-react'; 
+import {
+    Carousel
+  } from 'element-react';
+  
 import PropTypes from 'prop-types';
 import img from '../assets/pics/logo.jpg';
 import img2 from '../assets/pics/Logo2.jpg';
 
-const header = (props) => (
-    <Container>
-      <Grid container stackable verticalAlign='middle'>
-        <Grid.Row>
-          <Grid.Column>
-            <Transition.Group visible={props.visible} duration={ 0 }>
-              { props.visible && <Image src={ img } ></Image> }
-              { !props.visible && <Image src={ img2 } ></Image> }
-            </Transition.Group>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
+class HomeHeader extends Component {
+  render() {
+    return (
+      <Container>
+        <Grid container stackable verticalAlign='middle'>
+          <Grid.Row>
+            <Grid.Column>
+              <Carousel height="500px" indicatorPosition="outside">
+                <Carousel.Item>
+                  <Image src={img}></Image>
+                </Carousel.Item>
+                <Carousel.Item>
+                  <Image src={img2}></Image>
+                </Carousel.Item>
+              </Carousel>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
     </Container>
-);
+    );
+  }
+}
 
-header.propTypes = {
+HomeHeader.propTypes = {
     mobile: PropTypes.bool,
 };
 
-export default header;
+export default HomeHeader;
 
--- src/container/App.js
+++ src/container/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import Home from '../components/Home';
+import 'element-theme-default';
+
 
 class App extends Component {
   render() {
