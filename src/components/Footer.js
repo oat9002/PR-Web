89c6b124b21f5eb43583d673bc1dfc89ec55e3e8
Piinn <lin_loveorm@hotmail.com>
2018-07-17 11:32:54 +0700

    change color on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,81 +6,81 @@ const footer = () => (
         <Grid divided stackable>
           <Grid.Row>
             <Grid.Column width={5}>
-              <Header as='h4' content='Human Resource Division' />
+              <Header as='h4' inverted content='Human Resource Division' />
               <List link>
                 <List.Item
-                style ={{ 'color' : '#000000' }}>
+                style ={{ 'color' : '#FFFFFF' }}>
                 คณะการบริหารและจัดการ 
                 </List.Item>
                 
                 <List.Item
-                style ={{ 'color' : '#000000' }}>
+                style ={{ 'color' : '#FFFFFF' }}>
                 สถาบันเทคโนโลยีพระจอมเกล้าเจ้าคุณทหารลาดกระบัง
                 </List.Item>
                 
                 <List.Item
-                style ={{ 'color' : '#000000' }}>
+                style ={{ 'color' : '#FFFFFF' }}>
                 เลขที่ 1 ซอยฉลองกรุง 1 แขวงลาดกระบัง เขตลาดกระบัง กรุงเทพมหานคร 10520
                 </List.Item>
                 
                 <List.Item
-                style ={{ 'color' : '#000000' }}>
+                style ={{ 'color' : '#FFFFFF' }}>
                 เบอร์โทรศัพท์ : 02-329-8459-60, 02-329800
                 </List.Item>
                 
                 <List.Item
-                style ={{ 'color' : '#000000' }}> 
+                style ={{ 'color' : '#FFFFFF' }}> 
                 โทรสาร : 02-329-8461
                 </List.Item>
                 
                 <List.Item
-                style ={{ 'color' : '#000000' }}>
+                style ={{ 'color' : '#FFFFFF' }}>
                 E-mail: amc.kmitl@hotmail.com
                 </List.Item>
               </List>
             </Grid.Column>
             <Grid.Column width={5}>
-              <Header as='h4' content='Services' />
+              <Header as='h4' inverted content='Services' />
               <List link >
                 <List.Item as='a'
-                style ={{ 'color' : '#000000' }} >
+                style ={{ 'color' : '#FFFFFF' }} >
                 สมัครงาน
                 </List.Item>
 
                 <List.Item as='a'
-                style ={{ 'color' : '#000000' }}>
+                style ={{ 'color' : '#FFFFFF' }}>
                 พนักงานควรรู้
                 </List.Item>
 
                 <List.Item as='a'
-                style ={{ 'color' : '#000000' }}>
+                style ={{ 'color' : '#FFFFFF' }}>
                 สวัสดิการ
                 </List.Item>
 
                 <List.Item as='a'
-                style ={{ 'color' : '#000000' }}>
+                style ={{ 'color' : '#FFFFFF' }}>
                 พัฒนาบุคลากร<
                 /List.Item>
 
                 <List.Item as='a'
-                style ={{ 'color' : '#000000' }}>
+                style ={{ 'color' : '#FFFFFF' }}>
                 ความก้าวหน้า
                 </List.Item>
 
                 <List.Item as='a'
-                style ={{ 'color' : '#000000' }}>
+                style ={{ 'color' : '#FFFFFF' }}>
                 ประเมินผลการปฏิบัตงาน
                 </List.Item>
 
               </List>
             </Grid.Column>
             <Grid.Column width={5}>
-            <Header  as='h4' content='Go to' />
+            <Header  as='h4' inverted  content='Go to' />
               <List link >
-                <List.Item style ={{ 'color' : '#000000' }}>
+                <List.Item style ={{ 'color' : '#FFFFFF' }}>
                   <List.Icon name='internet explorer' />
                   <List.Content >
-                    <a href='//www.fam.kmitl.ac.th/' style ={{ 'color' : '#000000' }}>เว็บคณะการบริหารและจัดการ</a>
+                    <a href='//www.fam.kmitl.ac.th/' style ={{ 'color' : '#FFFFFF' }}>เว็บคณะการบริหารและจัดการ</a>
                   </List.Content>
                 </List.Item>
               </List>
