066a2b1d2adbc9b0e0965e5469816611826f9305
oat9002 <oat9002@hotmail.com>
2018-07-19 22:25:56 +0700

    Fixed gallery height

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -3,42 +3,31 @@ import { Modal, Image, Button, Transition } from 'semantic-ui-react';
 import { Carousel } from 'element-react';
 
 class Gallery extends Component {
-    state = {
-        visible: false
-    }
-
-    visibleHandler = () => {
-        this.setState({
-           visible: !this.state.visible
-        });
-    }
 
     render() {
         const mobileSize = `${ Math.ceil(window.innerHeight * 0.4) }px`;
         const desktopSize = `${ Math.ceil(window.innerHeight * 0.8) }px`;
 
-        return (
-            //<Transition visible={this.state.visible} animation='scale' duration={ 500 }>
-                <Modal
-                    trigger={ <Button size='small' basic color='blue'>คลิกเพื่อดูภาพ</Button> }
-                >
-                    <Modal.Content>
-                        <Carousel 
-                            height={ this.props.mobile ? mobileSize : desktopSize } 
-                            indicatorPosition="outside"
-                            trigger='click'
-                        >
-                            { 
-                                this.props.imgSrcs.map((src, idx) => (
-                                    <Carousel.Item key={ idx }>
-                                        <Image key={ idx } src={ src }></Image>
-                                    </Carousel.Item>
-                                ))
-                            }
-                        </Carousel>
-                    </Modal.Content>
-                </Modal>
-           // </Transition>
+        return ( 
+            <Modal
+                trigger={ <Button size='small' basic color='blue'>คลิกเพื่อดูภาพ</Button> }
+            >
+                <Modal.Content>
+                    <Carousel 
+                        height={ window.innerHeight > window.innerWidth ? mobileSize : desktopSize } 
+                        indicatorPosition="outside"
+                        trigger='click'
+                    >
+                        { 
+                            this.props.imgSrcs.map((src, idx) => (
+                                <Carousel.Item key={ idx }>
+                                    <Image key={ idx } src={ src }></Image>
+                                </Carousel.Item>
+                            ))
+                        }
+                    </Carousel>
+                </Modal.Content>
+            </Modal>
         );
     }
 }
