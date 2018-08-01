7c7b188ec9d2e8fa61af34db2afbbbca37d06d25
Piinn <lin_loveorm@hotmail.com>
2018-08-01 11:04:52 +0700

    edit words

--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -10,7 +10,7 @@ const newsGroup = (props) => {
           <Grid.Column width={ 7 }>
             <Item.Group>
                 <News title='การป้อนข้อมูลภาระงานสายวิชาการ' date='29' month='7' year='2561' icon='file' link='https://drive.google.com/open?id=1X5AK7829BW8-mydA7odqg0O7ggvj4LI1'></News>
-                <News title='ขอเชิญประชุมคณะกรรมการฝ่ายปริเมินผู้บริหาร' date='29' month='7' year='2561' icon='file' link='https://drive.google.com/open?id=1MMnMOpOiH_Sb1L5cHOW3uicSbqDa_DVe'></News>
+                <News title='ขอเชิญประชุมคณะกรรมการฝ่ายประเมินผู้บริหาร' date='29' month='7' year='2561' icon='file' link='https://drive.google.com/open?id=1MMnMOpOiH_Sb1L5cHOW3uicSbqDa_DVe'></News>
                 <News title='ประเมินคุณภาพบุคลากรสายวิชาการ (PBP)' date='29' month='7' year='2561' icon='file' link='https://drive.google.com/open?id=1UtRu-T6IekfyT8WPoBbVq1MTobWyNxiV'></News>
             </Item.Group>
           </Grid.Column>
--- src/container/DesktopContainer.js
+++ src/container/DesktopContainer.js
@@ -79,7 +79,7 @@ export default class DesktopContainer extends Component {
                             <Dropdown.Item as='a' >งานบริหารทรัพยากรบุคคล</Dropdown.Item>
                             <Dropdown.Item>งานบริหารสวัสดิการและสิทธิประโยชน์</Dropdown.Item>
                             <Dropdown.Item>งานยุทธศาสตร์และพัฒนาระบบบริหารทรัพยากรบุคคล</Dropdown.Item>
-                            <Dropdown simple item text='งานสร้างเศริมศักยาภาพและพัฒนาทรัพยากรบุคคล'>
+                            <Dropdown simple item text='งานสร้างเสริมศักยภาพและพัฒนาทรัพยากรบุคคล'>
                               <Dropdown.Menu>
                                 <Dropdown.Item as='a' >หลักสูตร/โครงสร้างการพัฒนาทรัพยากรบุคคล</Dropdown.Item>
                                 <Dropdown.Item>การประเมินผลการปฏิบัติงาน</Dropdown.Item>
@@ -94,14 +94,14 @@ export default class DesktopContainer extends Component {
                             <Dropdown.Item as='a' >แผนยุทธศาสตร์การบริหารทรัพยากรบุคคล(PDF)</Dropdown.Item>
                         </Dropdown.Menu>
                         </Dropdown>
-                        <Dropdown.Item as='a' >ค่านิยองค์กร</Dropdown.Item>
+                        <Dropdown.Item as='a' >ค่านิยมองค์กร</Dropdown.Item>
                       </Dropdown.Menu>
                     </Dropdown>
                   </Menu.Menu>
                   <Menu.Menu position='right'>
                     <Dropdown simple item text='สารสนเทศบุคลากร'>
                       <Dropdown.Menu>
-                        <Dropdown.Item as='a' >รายงานข้อมูลสารสนเทศบุคลกร</Dropdown.Item>
+                        <Dropdown.Item as='a' >รายงานข้อมูลสารสนเทศบุคลากร</Dropdown.Item>
                         <Dropdown.Item as='a' >แผนภาพและข้อมูลแสดงการกระจายอายุของบุคลากร</Dropdown.Item>
                       </Dropdown.Menu>
                     </Dropdown>
