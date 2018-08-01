f62ffd763c97b2c01d85308e11a02f30fb6bce6c
Piinn <lin_loveorm@hotmail.com>
2018-08-01 19:54:24 +0700

    Add new PDF and edit words

--- src/components/Development.js
+++ src/components/Development.js
@@ -7,7 +7,11 @@ const development = (props) => {
         <ReponsiveContainer>
             <Header as='h2' style={{ padding: '1em' }}content='พัฒนาบุคลากร' />
             <ul>
-            	<li><a href = '//drive.google.com/file/d/1eegpe7GZURfanPvt0C0GO6KLf_W9azN3/view?usp=sharing'>หลักเกณฑ์วิธี่การคิดภาระงานทางวิชาการของคณาจารย์-ส่วนกลาง</a></li>
+            	<li><a href = '//drive.google.com/file/d/1eegpe7GZURfanPvt0C0GO6KLf_W9azN3/view?usp=sharing'>หลักเกณฑ์วิธีการคิดภาระงานทางวิชาการของคณาจารย์-ส่วนกลาง</a></li>
+            	<li><a href = '//drive.google.com/file/d/1n7AYgOkaWPFROBguXDoxeTHnfu8JWwsY/view?usp=sharing'>ประกาศความสามารถภาษาอังกฤษของพนักงานสถาบัน</a></li><br />
+            	<li><a href = '//drive.google.com/file/d/1_6ond2CnIH8QDiL0unIY-inOnEqjBqH8/view?usp=sharing'>ประกาศ การบริหารงานบุคคลพนักงานสถาบัน</a></li><br />
+            	<li><a href = '//drive.google.com/file/d/14UYK216Q_kFuQphXx2iEBlzqZOW6NbRv/view?usp=sharing'>ประกาศ หลักเกณฑ์ วิธีการ และเงื่อนไขการ</a></li><br />
+
            </ul>
         </ReponsiveContainer>
     );
--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -12,6 +12,13 @@ const newsGroup = (props) => {
                 <News title='การป้อนข้อมูลภาระงานสายวิชาการ' date='29' month='7' year='2561' icon='file' link='https://drive.google.com/open?id=1X5AK7829BW8-mydA7odqg0O7ggvj4LI1'></News>
                 <News title='ขอเชิญประชุมคณะกรรมการฝ่ายประเมินผู้บริหาร' date='29' month='7' year='2561' icon='file' link='https://drive.google.com/open?id=1MMnMOpOiH_Sb1L5cHOW3uicSbqDa_DVe'></News>
                 <News title='ประเมินคุณภาพบุคลากรสายวิชาการ (PBP)' date='29' month='7' year='2561' icon='file' link='https://drive.google.com/open?id=1UtRu-T6IekfyT8WPoBbVq1MTobWyNxiV'></News>
+                <News title='ประกาศ ก.พ.อ. การกำหนดชื่อสาขาวิชาสำหรับการเสนอขอกำหนดตำแหน่งทางวิชาการและการเทียบเคียงสาขาวิชาที่เคยกำหนดไปแล้ว' date='1' month='8' year='2561' icon='file' link='https://drive.google.com/file/d/1J-OY03WpoPlqQQO-viHKd5Yn1F7smIyv/view?usp=sharing'></News>
+                <News title='ประกาศสวัสดิการของพนักงานสถาบันเงินงบประมาณ' date='1' month='8' year='2561' icon='file' link='https://drive.google.com/open?id=1UtRu-T6IekfyT8WPoBbVq1MTobWyNxiV'></News>
+                <News title='ประกาศ หลักเกณฑ์ วิธีการ และเงื่อนไขการต่อเวลาราชการของข้าราชการพลเรือนในสถาบีนอุดมศึกษา' date='1' month='8' year='2561' icon='file' link=https://drive.google.com/file/d/1O8kW-N-m2WFpK8UfZ9Z8gYYBDMzScqms/view?usp=sharing'></News>
+                <News title='ประกาศความสามารถภาษาอังกฤษของพนักงานสถาบัน' date='1' month='8' year='2561' icon='file' link='https://drive.google.com/file/d/1kevfSsHYtO-gVUldMbsdZH_wiLTEswyo/view?usp=sharing'></News>
+                <News title='ประกาศ การบริหารงานบุคคลพนักงานสถาบันประเภทพิเศษ' date='1' month='8' year='2561' icon='file' link='https://drive.google.com/file/d/1MnRQFuUvuDXS8l9DOjLiY9tT8Llja9t5/view?usp=sharing'></News>
+                <News title='ประกาศ วัน เวลา ทำงานและวิธีการลงเวลาทำงาน' date='1' month='8' year='2561' icon='file' link='https://drive.google.com/file/d/15Sw9ljLVEKxUcHIjnnHYrBu616kAFF59/view?usp=sharing'></News>
+                
             </Item.Group>
           </Grid.Column>
         </Grid.Row>
