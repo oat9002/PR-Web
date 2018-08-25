060a38195121fac0efa819d101a820857285a64f
oat9002 <oat9002@hotmail.com>
2018-08-25 21:14:26 +0700

    Change news and galleries color

--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -9,57 +9,12 @@ const newsGroup = (props) => {
           <Grid.Column width={ 14 }>
             <Item.Group>
                 <News 
-                    title='การป้อนข้อมูลภาระงานสายวิชาการ' 
-                    date='29' 
-                    month='7' 
-                    year='2561' 
-                    icon='file' 
-                    link='https://drive.google.com/open?id=1X5AK7829BW8-mydA7odqg0O7ggvj4LI1' />
-                <News 
-                    title='ขอเชิญประชุมคณะกรรมการฝ่ายประเมินผู้บริหาร' 
-                    date='29' 
-                    month='7' 
-                    year='2561' 
-                    icon='file' 
-                    link='https://drive.google.com/open?id=1MMnMOpOiH_Sb1L5cHOW3uicSbqDa_DVe' />
-                <News 
-                    title='ประเมินคุณภาพบุคลากรสายวิชาการ (PBP)' 
-                    date='29' 
-                    month='7'
-                    year='2561' 
-                    icon='file' 
-                    link='https://drive.google.com/open?id=1UtRu-T6IekfyT8WPoBbVq1MTobWyNxiV'/>
-                <News 
-                    title='ประกาศ ก.พ.อ. การกำหนดชื่อสาขาวิชาสำหรับการเสนอขอกำหนดตำแหน่งทางวิชาการ' 
-                    date='1'
-                    month='8' 
-                    year='2561' 
-                    icon='file' 
-                    link='https://drive.google.com/file/d/1J-OY03WpoPlqQQO-viHKd5Yn1F7smIyv/view?usp=sharing' 
-                    isNew={ true }/>
-                <News 
-                    title='ประกาศสวัสดิการของพนักงานสถาบันเงินงบประมาณ'
-                    date='1'
-                    month='8' 
-                    year='2561' 
-                    icon='file' 
-                    link='https://drive.google.com/open?id=1UtRu-T6IekfyT8WPoBbVq1MTobWyNxiV' 
-                    isNew={ true }/>
-                <News 
-                    title='ประกาศ หลักเกณฑ์ วิธีการ และเงื่อนไขการต่อเวลาราชการของข้าราชการพลเรือนในสถาบันอุดมศึกษา' 
+                    title='ประกาศ วัน เวลา ทำงานและวิธีการลงเวลาทำงาน' 
                     date='1' 
-                    month='8' 
+                    month='8'
                     year='2561' 
                     icon='file' 
-                    link='https://drive.google.com/file/d/1O8kW-N-m2WFpK8UfZ9Z8gYYBDMzScqms/view?usp=sharing' 
-                    isNew={ true }/>
-                <News 
-                    title='ประกาศความสามารถภาษาอังกฤษของพนักงานสถาบัน' 
-                    date='1' 
-                    month='8' 
-                    year='2561' 
-                    icon='file' 
-                    link='https://drive.google.com/file/d/1kevfSsHYtO-gVUldMbsdZH_wiLTEswyo/view?usp=sharing' 
+                    link='https://drive.google.com/file/d/15Sw9ljLVEKxUcHIjnnHYrBu616kAFF59/view?usp=sharing' 
                     isNew={ true }/>
                 <News 
                     title='ประกาศ การบริหารงานบุคคลพนักงานสถาบันประเภทพิเศษ' 
@@ -70,13 +25,60 @@ const newsGroup = (props) => {
                     link='https://drive.google.com/file/d/1MnRQFuUvuDXS8l9DOjLiY9tT8Llja9t5/view?usp=sharing' 
                     isNew={ true }/>
                 <News 
-                    title='ประกาศ วัน เวลา ทำงานและวิธีการลงเวลาทำงาน' 
+                    title='ประกาศความสามารถภาษาอังกฤษของพนักงานสถาบัน' 
                     date='1' 
-                    month='8'
+                    month='8' 
                     year='2561' 
                     icon='file' 
-                    link='https://drive.google.com/file/d/15Sw9ljLVEKxUcHIjnnHYrBu616kAFF59/view?usp=sharing' 
+                    link='https://drive.google.com/file/d/1kevfSsHYtO-gVUldMbsdZH_wiLTEswyo/view?usp=sharing' 
                     isNew={ true }/>
+                <News 
+                    title='ประกาศ หลักเกณฑ์ วิธีการ และเงื่อนไขการต่อเวลาราชการของข้าราชการพลเรือนในสถาบันอุดมศึกษา' 
+                    date='1' 
+                    month='8' 
+                    year='2561' 
+                    icon='file' 
+                    link='https://drive.google.com/file/d/1O8kW-N-m2WFpK8UfZ9Z8gYYBDMzScqms/view?usp=sharing' 
+                    isNew={ true }/>
+                <News 
+                    title='ประกาศสวัสดิการของพนักงานสถาบันเงินงบประมาณ'
+                    date='1'
+                    month='8' 
+                    year='2561' 
+                    icon='file' 
+                    link='https://drive.google.com/open?id=1UtRu-T6IekfyT8WPoBbVq1MTobWyNxiV' 
+                    isNew={ true }/>
+                <News 
+                    title='ประกาศ ก.พ.อ. การกำหนดชื่อสาขาวิชาสำหรับการเสนอขอกำหนดตำแหน่งทางวิชาการ' 
+                    date='1'
+                    month='8' 
+                    year='2561' 
+                    icon='file' 
+                    link='https://drive.google.com/file/d/1J-OY03WpoPlqQQO-viHKd5Yn1F7smIyv/view?usp=sharing' 
+                    isNew={ true }/>
+                <News 
+                    title='ประเมินคุณภาพบุคลากรสายวิชาการ (PBP)' 
+                    date='29' 
+                    month='7'
+                    year='2561' 
+                    icon='file' 
+                    link='https://drive.google.com/open?id=1UtRu-T6IekfyT8WPoBbVq1MTobWyNxiV'/>
+                <News 
+                    title='ขอเชิญประชุมคณะกรรมการฝ่ายประเมินผู้บริหาร' 
+                    date='29' 
+                    month='7' 
+                    year='2561' 
+                    icon='file' 
+                    link='https://drive.google.com/open?id=1MMnMOpOiH_Sb1L5cHOW3uicSbqDa_DVe' />
+                <News 
+                    title='การป้อนข้อมูลภาระงานสายวิชาการ' 
+                    date='29' 
+                    month='7' 
+                    year='2561' 
+                    icon='file' 
+                    link='https://drive.google.com/open?id=1X5AK7829BW8-mydA7odqg0O7ggvj4LI1' />
+               
+                
             </Item.Group>
           </Grid.Column>
         </Grid.Row>
