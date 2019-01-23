22878963c6ec819b569b6fd30b30b5294955ae14
pinyaratCh <lin_lovorm@hotmail.com>
2019-01-23 11:24:55 +0700

    กองทุนสำรองเลี้ยงชีพ

--- src/components/NewsCards.js
+++ src/components/NewsCards.js
@@ -12,6 +12,17 @@ const newsCards = (props) => {
             <Grid.Row centered>
 <Grid.Column width={ newsWidth }>
                     <News 
+                        title='กองทุนสำรองเลี้ยงชีพ' 
+                        date='23' 
+                        month='1' 
+                        year='2562' 
+                        imgSrc={ images['new02.jpg'] }
+                        link='http://hr.kmitl.ac.th/wp-content/uploads/2019/01/%E0%B9%81%E0%B8%9A%E0%B8%9A%E0%B8%9F%E0%B8%AD%E0%B8%A3%E0%B9%8C%E0%B8%A1-%E0%B9%83%E0%B8%9A%E0%B8%AA%E0%B8%A1%E0%B8%B1%E0%B8%84%E0%B8%A3%E0%B8%AA%E0%B8%A1%E0%B8%B2%E0%B8%8A%E0%B8%B4%E0%B8%81%E0%B8%81%E0%B8%AD%E0%B8%87%E0%B8%97%E0%B8%B8%E0%B8%99%E0%B8%AA%E0%B8%B3%E0%B8%A3%E0%B8%AD%E0%B8%87%E0%B9%80%E0%B8%A5%E0%B8%B5%E0%B9%89%E0%B8%A2%E0%B8%87%E0%B8%8A%E0%B8%B5%E0%B8%9E-TMB.pdf' 
+                        isNew={ true }
+                        card />
+                </Grid.Column>
+<Grid.Column width={ newsWidth }>
+                    <News 
                         title='ประกาศผลสอบแข่งขันบุคลากรประจำสถาบันสายสนับสนุนวิชาการประจำปีการศึกษา 2562' 
                         date='23' 
                         month='1' 
